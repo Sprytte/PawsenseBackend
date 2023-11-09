1d7d0e82c41391a92f44ef8673840a4027e02d5b
ayasophya <jadekamsing@gmail.com>
2023-11-08 22:39:15 -0500

    Weight graph done for lab 4

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
 import time
-from flask import Flask, request
-from flask_cors import CORS
+from datetime import datetime
+from flask import Flask, request, render_template
+#from flask_cors import CORS
 from databasesetup import *
 
 app = Flask(__name__)
-CORS(app)
+#CORS(app)
+
+time_data = []
+weight_data = []
+def read_data(file):
+
+    with open(file, 'r') as file:
+        for line in file:
+            parts = line.split(',')
+            for part in parts:
+                if part.strip().startswith("time:"):
+                    time = part.strip().replace("time:", "").strip()
+                    time_data.append(time)
+                elif part.strip().startswith("weight:"):
+                    weight = float(part.strip().replace("weight:", "").strip())
+                    weight_data.append(weight)
+
+data_file = 'weight_data.txt'
+read_data(data_file)
+@app.route('/Weights')
+def weight():
+    labels = [datetime.strptime(time, '%Y-%m-%d %H:%M:%S.%f').strftime('%Y-%m-%d %I:%M %p') for time in time_data]
+    data = weight_data
+    header = "This is the weight line graph"
+    return render_template('weight_line_graph.html', data=data, labels=labels, header=header)
 
 @app.route('/time')
 def get_current_time():
@@ -22,8 +47,8 @@ def upload_data():
     return 'Data received successfully'
 
 # create_connection(r"C:\sqlite\db\pawsense.db")
-#
-# create_connection(r"PawsenseDB")
+
+create_connection(r"PawsenseDB")
 if __name__ == '__main__':
     app.run()
 
